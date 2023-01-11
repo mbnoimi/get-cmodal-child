bcd7b9042ff330679f467e9e60c0f7319d9f4722
mbnoimi <mbnoimi@gmail.com>
2023-01-12 00:14:07 +0300

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-btn
+      id="show-modal"
+      @click="showModal = true"
+      color="primary"
+      icon="mail"
+      label="Show Modal"
+    />
+
+    <Teleport to="body">
+      <!-- use the modal component, pass in the prop -->
+      <modal :show="showModal" @close="showModal = false">
+        <template #header>
+          <q-btn id="jassem" label="Check Port" ref="focusSubmit" />
+        </template>
+      </modal>
+    </Teleport>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Modal from 'src/components/ModalDialog.vue';
+
+const showModal = ref(false);
+const focusSubmit = ref(null);
+
+onMounted(() => {
+  ////// ERROR
+  console.log(focusSubmit.value.label);
+});
+</script>
---
+++ src/pages/IndexPage_.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-btn id="jassem" label="Check Port" ref="focusSubmit" />
+  </q-page>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+const focusSubmit = ref(null);
+onMounted(() => {
+  console.log(focusSubmit.value);
+  console.log(focusSubmit.value.label);
+});
+</script>
